ab7050d0be14d97d9fb2b34367319e646712eb18
Kristians <murdskristians@gmail.com>
2023-10-19 18:55:18 +0300

    finished mobile

--- index.php
+++ index.php
@@ -21,6 +21,31 @@
 
 <div id="bottom-container">
 
+    <div id="logo-1" class="logos"></div>
+    <div id="logo-2" class="logos"></div>
+    <div id="logo-3" class="logos"></div>
+    <div id="logo-4" class="logos"></div>
+    <div id="logo-5" class="logos"></div>
+    <div id="logo-6" class="logos"></div>
+    <div id="logo-7" class="logos"></div>
+    <div id="logo-8" class="logos"></div>
+
+    <div id="qr-code" class="logos"></div>
+
+    <div id="stylish-btn-2" class="stylish-btns-class">SHOP</div>
+    <div id="stylish-btn-3" class="stylish-btns-class">ABOUT US</div>
+
+<div id="social-icons-container">
+    <div id="icon-1" class="social-icons"></div>
+    <div id="icon-2" class="social-icons"></div>
+    <div id="icon-3" class="social-icons"></div>
+</div>
+
+    <div id="rights-reserved">
+        <p>All rights reserved.</p>
+        <p>Statue of Culture 2023</p>
+    </div>
+
 </div>
 
 
