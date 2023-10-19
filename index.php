b6d5c797d0eb1d45f356c07a03141b949bcbaec8
Kristians <murdskristians@gmail.com>
2023-10-19 20:58:20 +0300

    finished version 1

--- index.php
+++ index.php
@@ -32,7 +32,20 @@
         <div id="logo-8" class="logos"></div>
     </div>
 
-    <div id="qr-code" class="logos"></div>
+    <div id="qr-code-container">
+        <div id="qr-code" class="logos"></div>
+
+        <div id="arrow-with-text">
+            <svg id="arrow-left" preserveAspectRatio="none" data-bbox="65.5 20 69 160" viewBox="65.5 20 69 160" height="200" width="200" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true">
+                <g>
+                    <path d="M103.083 23.463L100.001 20l-3.082 3.463-25.764 28.948-5.655 6.773h20.536V180h27.928V59.184H134.5l-31.417-35.721zm6.774 31.665v120.79H90.143V55.128H74.237l25.765-28.948 25.762 28.948h-15.907z"></path>
+                </g>
+            </svg>
+
+            <p id="tap-to-join">TAP TO JOIN</p>
+        </div>
+    </div>
+
 
     <div id="stylish-btn-2" class="stylish-btns-class">SHOP</div>
     <div id="stylish-btn-3" class="stylish-btns-class">ABOUT US</div>
