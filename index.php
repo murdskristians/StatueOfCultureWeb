c719b1b3ca440e0a0e5126674f54295cdd67dc41
Kristians <murdskristians@gmail.com>
2023-10-19 17:02:21 +0300

    finished top container

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
 
-echo "Hello World!";
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>JuicyGrow</title>
+    <link rel="stylesheet" href="/styles/styles.css">
+
+</head>
+<body>
+
+<div id="top-container">
+    <div id="turning-head"></div>
+    <div id="stylish-btn">JOIN CULTURE CLUB</div>
+</div>
+
+
+<div id="bottom-container">
+
+</div>
+
+
+
+</body>
