d96673de336aa659c5c4a3c2f93a75a550874fcc
Kristians <murdskristians@gmail.com>
2023-10-19 20:17:42 +0300

    working on desktop version

--- index.php
+++ index.php
@@ -21,33 +21,37 @@
 
 <div id="bottom-container">
 
-    <div id="logo-1" class="logos"></div>
-    <div id="logo-2" class="logos"></div>
-    <div id="logo-3" class="logos"></div>
-    <div id="logo-4" class="logos"></div>
-    <div id="logo-5" class="logos"></div>
-    <div id="logo-6" class="logos"></div>
-    <div id="logo-7" class="logos"></div>
-    <div id="logo-8" class="logos"></div>
+    <div id="logos-wrapper">
+        <div id="logo-1" class="logos"></div>
+        <div id="logo-2" class="logos"></div>
+        <div id="logo-3" class="logos"></div>
+        <div id="logo-4" class="logos"></div>
+        <div id="logo-5" class="logos"></div>
+        <div id="logo-6" class="logos"></div>
+        <div id="logo-7" class="logos"></div>
+        <div id="logo-8" class="logos"></div>
+    </div>
 
     <div id="qr-code" class="logos"></div>
 
     <div id="stylish-btn-2" class="stylish-btns-class">SHOP</div>
     <div id="stylish-btn-3" class="stylish-btns-class">ABOUT US</div>
 
-<div id="social-icons-container">
-    <div id="icon-1" class="social-icons"></div>
-    <div id="icon-2" class="social-icons"></div>
-    <div id="icon-3" class="social-icons"></div>
-</div>
+    <div id="social-icons-container">
+        <div id="icon-1" class="social-icons"></div>
+        <div id="icon-2" class="social-icons"></div>
+        <div id="icon-3" class="social-icons"></div>
+    </div>
 
+    <div id="copy-right">
+        <p>Copyright © 2023</p>
+    </div>
     <div id="rights-reserved">
-        <p>All rights reserved.</p>
+        <p>All rights reserved. </p>
         <p>Statue of Culture 2023</p>
     </div>
 
 </div>
-
 
 
 </body>
