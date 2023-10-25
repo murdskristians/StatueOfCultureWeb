7fb9beb12b637121c50c4e24f5eb8b33b350c001
Emil <emils.boreiko@gmail.com>
2023-10-25 22:48:29 +0300

    finished statueofculture

--- pages/projects/project-1.php
+++ pages/projects/project-1.php
@@ -15,47 +15,47 @@
     <a id="project-1-logo" class="logos"></a>
 
     <div id="project-1-gallery">
-
-        <div id="project-1-gallery-row-1" class="project-1-gallery-row">
-            <div id="project-1-gallery-row-1-col-1" class="project-1-gallery-col">
-                <img src="/images/project-1/jacket1.webp" alt="project-1-img-1">
-            </div>
-            <div id="project-1-gallery-row-1-col-2" class="project-1-gallery-col">
-                <img src="/images/project-1/jacket2.webp" alt="project-1-img-2">
-            </div>
-        </div>
-
-        <div id="project-1-gallery-text"><p class="project-1-gallery-texts-p">SEASON 1</p></div>
-
-        <div id="project-1-gallery-row-2" class="project-1-gallery-row">
-            <div id="project-1-gallery-row-2-col-1" class="project-1-gallery-col">
-                <video src="/images/project-1/project-1-left.mp4" controls autoplay></video>
-            </div>
-            <div id="project-1-gallery-row-2-col-2" class="project-1-gallery-col">
-                <video src="/images/project-1/project-1-middle.mp4" controls autoplay></video>
-            </div>
-            <div id="project-1-gallery-row-2-col-3" class="project-1-gallery-col">
-                <video src="/images/project-1/project-1-right.mp4" controls autoplay></video>
-            </div>
-        </div>
-
-        <div id="project-1-gallery-row-3" class="project-1-gallery-row">
-            <div id="project-1-gallery-row-3-col-1" class="project-1-gallery-col">
-                <img src="/images/project-1/pants.webp" alt="project-1-img-7">
-            </div>
-            <div id="project-1-gallery-inner-row">
-                <div id="project-1-gallery-row-3-col-2" class="project-1-gallery-col">
-                    <img src="/images/project-1/jacket3.webp" alt="project-1-img-8">
+        <div id="project-1-gallery-wrapper">
+            <div id="project-1-gallery-row-1" class="project-1-gallery-row">
+                <div id="project-1-gallery-row-1-col-1" class="project-1-gallery-col">
+<!--                    <img src="/images/project-1/jacket1.webp" alt="project-1-img-1">-->
                 </div>
-                <div id="project-1-gallery-row-3-col-3" class="project-1-gallery-col">
-                    <img src="/images/project-1/pants2.webp" alt="project-1-img">
+                <div id="project-1-gallery-row-1-col-2" class="project-1-gallery-col">
+<!--                    <img src="/images/project-1/jacket2.webp" alt="project-1-img-2">-->
                 </div>
             </div>
+
+            <div id="project-1-gallery-text"><p class="project-1-gallery-texts-p">SEASON 1</p></div>
+
+            <div id="project-1-gallery-row-2" class="project-1-gallery-row">
+                <div id="project-1-gallery-row-2-col-1" class="project-1-gallery-col">
+                    <video src="/images/project-1/project-1-left.mp4" autoplay muted loop></video>
+                </div>
+                <div id="project-1-gallery-row-2-col-2" class="project-1-gallery-col">
+                    <video src="/images/project-1/project-1-middle.mp4" autoplay muted loop></video>
+                </div>
+                <div id="project-1-gallery-row-2-col-3" class="project-1-gallery-col">
+                    <video src="/images/project-1/project-1-right.mp4" autoplay muted loop></video>
+                </div>
+            </div>
+
+            <div id="project-1-gallery-row-3" class="project-1-gallery-row">
+                <div id="project-1-gallery-row-3-col-1" class="project-1-gallery-col">
+<!--                    <img src="/images/project-1/pants.webp" alt="project-1-img-7">-->
+                </div>
+                <div id="project-1-gallery-inner-row">
+                    <div id="project-1-gallery-row-3-col-2" class="project-1-gallery-col">
+<!--                        <img src="/images/project-1/jacket3.webp" alt="project-1-img-8">-->
+                    </div>
+                    <div id="project-1-gallery-row-3-col-3" class="project-1-gallery-col">
+<!--                        <img src="/images/project-1/pants2.webp" alt="project-1-img">-->
+                    </div>
+                </div>
+            </div>
+
+            <div id="project-1-gallery-text-2"><p class="project-1-gallery-texts-p">STATUE OF CULTURE</p></div>
+
         </div>
-
-        <div id="project-1-gallery-text-2"><p class="project-1-gallery-texts-p">STATUE OF CULTURE</p></div>
-
-
     </div>
 
     <a id="project-1-home-btn" class="stylish-btns-class common-btn home-btn"  href="../../index.php">HOME</a>
