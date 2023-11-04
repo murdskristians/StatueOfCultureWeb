1b673a217d1aae4d97b0a904b07471073273ce6c
Emils <emils.boreiko@gmail.com>
2023-11-04 23:50:09 +0200

    gg

--- index.php
+++ index.php
@@ -8,66 +8,92 @@
 <head>
     <meta charset="UTF-8">
     <title>StatueOfCulture</title>
-    <link rel="stylesheet" href="/styles/styles.css">
+<!--    <link rel="stylesheet" href="/styles/styles.css">-->
+    <style>
+        html,body{
+            width:100%;
+            /*height:100%;*/
+            margin:0;
+        }
+        .frame_container{
+            width:100%;
+            margin:auto;
+            height:100%;
+            position: relative;
+        }
+        .image_container{
+            background-image: url("/images/amiri_opt.jpg");
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: top;
+            /*height:100%;*/
+            width: 100%;
+        }
+        .main_img{
+            width:100%;
+            transition:opacity 0.5s ease;
+        }
 
+        .sensor_div{
+            /*background-color: rgba(255,255,255,0.3);*/
+            cursor: pointer;
+            z-index: 99;
+            position: absolute;
+        }
+        #shop_1_btn{
+            width: 36%;
+            height: 9%;
+            top: 18%;
+            right: 7%;
+        }
+
+        #shop_2_btn{
+            width: 30%;
+            height: 19%;
+            top: 26%;
+            left: 0%;
+        }
+
+        #img_1{
+            z-index: 0;
+        }
+        #img_2{
+            z-index: 1;
+            position: absolute;
+            height: 1000px;
+            left: 0;
+            top: 0;
+            height: 100%;
+        }
+
+            /*position:absolute;*/
+
+    </style>
 </head>
+
 <body>
+    <div class="frame_container">
+        <img src="/images/amiri_opt.jpg" alt="" class="main_img" id="img_1">
+        <img src="/images/gallery_opt.jpg" alt="" class="main_img" id="img_2">
+<!--        <img src="/images/amiri_opt.jpg" alt="" class="main_img" id="img_1">-->
 
-<div id="top-container">
-    <div id="turning-head"></div>
-<!--    <div id="stylish-btn">JOIN CULTURE CLUB</div>-->
-    <button id="stylish-btn" class="smooth-button">JOIN CULTURE CLUB</button>
-
-</div>
-
-
-<div id="bottom-container">
-
-    <div id="logos-wrapper">
-        <a id="logo-1" class="logos" href="pages/projects/project-1.php"></a>
-        <a id="logo-2" class="logos"></a>
-        <a id="logo-3" class="logos"></a>
-        <a id="logo-4" class="logos"></a>
-        <a id="logo-5" class="logos"></a>
-        <a id="logo-6" class="logos"></a>
-        <a id="logo-7" class="logos"></a>
-        <a id="logo-8" class="logos"></a>
+        <div class="sensor_div" id="shop_1_btn"></div>
+        <div class="sensor_div" id="shop_2_btn"></div>
+<!--        <div class="image_container">-->
+<!---->
+<!--        </div>-->
     </div>
 
-    <div id="qr-code-container">
-<!--        <div id="qr-code" class="logos"></div>-->
-
-        <a id="qr-code" class="logos" href="https://t.me/StatueofCulture"></a>
-        <div id="arrow-with-text">
-            <svg id="arrow-left" preserveAspectRatio="none" data-bbox="65.5 20 69 160" viewBox="65.5 20 69 160" height="200" width="200" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true">
-                <g>
-                    <path d="M103.083 23.463L100.001 20l-3.082 3.463-25.764 28.948-5.655 6.773h20.536V180h27.928V59.184H134.5l-31.417-35.721zm6.774 31.665v120.79H90.143V55.128H74.237l25.765-28.948 25.762 28.948h-15.907z"></path>
-                </g>
-            </svg>
-
-            <p id="tap-to-join">TAP TO JOIN</p>
-        </div>
-    </div>
-
-
-    <a id="stylish-btn-2" class="stylish-btns-class common-btn" href="https://t.me/StatueofCulture">SHOP</a>
-    <a id="stylish-btn-3" class="stylish-btns-class common-btn" href="./pages/about-us.php">ABOUT US</a>
-
-    <div id="social-icons-container">
-        <a id="icon-1" class="social-icons" href="https://www.instagram.com/statueofculture/?igshid=YmMyMTA2M2Y%3D"></a>
-        <a id="icon-2" class="social-icons" href="https://www.depop.com/statueofculture/"></a>
-        <a id="icon-3" class="social-icons" href="https://www.grailed.com/statueofculture?_branch_match_id=1112866068434094352&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXTy9KzMxJTdFLLCjQy8nMy9ZPdEv2cnRLcc4tTQIAKQTGeyQAAAA%3D"></a>
-    </div>
-
-    <div id="copy-right">
-        <p>Copyright © 2023</p>
-    </div>
-    <div id="rights-reserved">
-        <p>All rights reserved. </p>
-        <p>Statue of Culture 2023</p>
-    </div>
-
-</div>
-
-<script src="js/scripts.js?<?php echo microtime(); ?>"></script>
+    <script>
+        document.getElementById("shop_2_btn").addEventListener("mouseover", function(){
+            let img = document.getElementById("img_2");
+            img.style.opacity = "0";
+            img.style.pointerEvents = "none";
+        });
+        document.getElementById("shop_2_btn").addEventListener("mouseout", function(){
+            let img = document.getElementById("img_2");
+            img.style.opacity = "1";
+            img.style.pointerEvents = "none";
+        });
+    </script>
 </body>
---
+++ previous.php
@@ -0,0 +1,73 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>StatueOfCulture</title>
+    <link rel="stylesheet" href="/styles/styles.css">
+
+</head>
+<body>
+
+<div id="top-container">
+    <div id="turning-head"></div>
+<!--    <div id="stylish-btn">JOIN CULTURE CLUB</div>-->
+    <button id="stylish-btn" class="smooth-button">JOIN CULTURE CLUB</button>
+
+</div>
+
+
+<div id="bottom-container">
+
+    <div id="logos-wrapper">
+        <a id="logo-1" class="logos" href="pages/projects/project-1.php"></a>
+        <a id="logo-2" class="logos"></a>
+        <a id="logo-3" class="logos"></a>
+        <a id="logo-4" class="logos"></a>
+        <a id="logo-5" class="logos"></a>
+        <a id="logo-6" class="logos"></a>
+        <a id="logo-7" class="logos"></a>
+        <a id="logo-8" class="logos"></a>
+    </div>
+
+    <div id="qr-code-container">
+<!--        <div id="qr-code" class="logos"></div>-->
+
+        <a id="qr-code" class="logos" href="https://t.me/StatueofCulture"></a>
+        <div id="arrow-with-text">
+            <svg id="arrow-left" preserveAspectRatio="none" data-bbox="65.5 20 69 160" viewBox="65.5 20 69 160" height="200" width="200" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true">
+                <g>
+                    <path d="M103.083 23.463L100.001 20l-3.082 3.463-25.764 28.948-5.655 6.773h20.536V180h27.928V59.184H134.5l-31.417-35.721zm6.774 31.665v120.79H90.143V55.128H74.237l25.765-28.948 25.762 28.948h-15.907z"></path>
+                </g>
+            </svg>
+
+            <p id="tap-to-join">TAP TO JOIN</p>
+        </div>
+    </div>
+
+
+    <a id="stylish-btn-2" class="stylish-btns-class common-btn" href="https://t.me/StatueofCulture">SHOP</a>
+    <a id="stylish-btn-3" class="stylish-btns-class common-btn" href="./pages/about-us.php">ABOUT US</a>
+
+    <div id="social-icons-container">
+        <a id="icon-1" class="social-icons" href="https://www.instagram.com/statueofculture/?igshid=YmMyMTA2M2Y%3D"></a>
+        <a id="icon-2" class="social-icons" href="https://www.depop.com/statueofculture/"></a>
+        <a id="icon-3" class="social-icons" href="https://www.grailed.com/statueofculture?_branch_match_id=1112866068434094352&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXTy9KzMxJTdFLLCjQy8nMy9ZPdEv2cnRLcc4tTQIAKQTGeyQAAAA%3D"></a>
+    </div>
+
+    <div id="copy-right">
+        <p>Copyright © 2023</p>
+    </div>
+    <div id="rights-reserved">
+        <p>All rights reserved. </p>
+        <p>Statue of Culture 2023</p>
+    </div>
+
+</div>
+
+<script src="js/scripts.js?<?php echo microtime(); ?>"></script>
+</body>
