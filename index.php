07a3dc2078289f08688c4e3a90fba8d0be36ec35
Emils <emils.boreiko@gmail.com>
2023-11-05 23:28:14 +0200

    ggg

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
             width:100%;
             /*height:100%;*/
             margin:0;
+            font-family: 'Roboto', sans-serif;
         }
         .frame_container{
             width:100%;
@@ -123,11 +124,144 @@
             transition:opacity 0.5s ease;
         }
 
+        .black_block{
+            width:100%;
+            background-color: rgba(0,0,0,1);
+            margin-top:-6px;
+        }
 
+        .footer_text{
+            color:white;
+            text-align: center;
+            font-family: 'Roboto', sans-serif;
+            font-size: 1rem;
+            padding-bottom:15px;
+            padding-top:15px;
+        }
 
+        .top_button_wrap{
+            width:80%;
+            margin:auto;
+            display: flex;
+            justify-content: center;
+            font-size: 1.5rem;
+        }
 
+        .top_button{
+            border-bottom:1px solid gray;
+            color:white;
+            flex:1;
+            text-align: center;
+            padding-top: 30px;
+            padding-bottom: 22px;
+            cursor: pointer;
+            user-select: none;
+        }
 
+        .top_button.active{
+            border-bottom:5px solid white;
+        }
+        .post_gallery_image{
+            background-image: url("/images/test_img_1.jpg");
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            width:100%;
+            height:100%;
+        }
+        .blog_list_wrap{
+            height:50vh;
+            color:white;
+            margin-top:35px;
+            margin-bottom:35px;
+        }
+        .blog_post{
+            height:100%;
+            display: flex;
+            padding-right:25px;
+            position: relative;
+        }
+        .post_text_block_wrap{
+            flex:1;
+            padding-left:25px;
+            position: relative;
+        }
+        .post_gallery{
+            flex:1;
+            position: relative;
+        }
+        .post_gallery_btn{
 
+        }
+        .tag_text{
+            color:royalblue;
+            margin-bottom:15px;
+            font-size: 1.2rem;
+        }
+        .post_title {
+            font-size: 2.5rem;
+            margin-bottom: 15px;
+        }
+        .post_description{
+            color:lightgray;
+            font-size: 1.2rem;
+        }
+        .post_change_button_wrap{
+            position: absolute;
+            bottom: 0;
+            left: 25px;
+            display: flex;
+        }
+        .post_change_button{
+            height:45px;
+            width:45px;
+            background-position: center;
+            background-size: contain;
+            background-repeat: no-repeat;
+            transition: all 0.5s ease;
+            cursor: pointer;
+        }
+
+        .post_change_button.left{
+            background-image: url("/images/button_big.png");
+            transform: rotate(180deg);
+        }
+        .post_change_button.right{
+            background-image: url("/images/button_big.png");
+            margin-left:20px;
+        }
+        .post_change_button.left:hover{
+            transform: translateX(-5px) rotate(180deg);
+        }
+        .post_change_button.right:hover{
+            transform: translateX(5px);
+        }
+        .post_gallery_btn{
+            height:45px;
+            width:45px;
+            background-position: center;
+            background-size: contain;
+            background-repeat: no-repeat;
+            transition: all 0.5s ease;
+            cursor: pointer;
+            position: absolute;
+            top:50%;
+        }
+        .post_gallery_btn.left{
+            background-image: url("/images/button.png");
+            transform: translateY(-50%) rotate(180deg);
+            left:10px;
+        }
+        .post_gallery_btn.right{
+            background-image: url("/images/button.png");
+            right:10px;
+        }
+        .post_gallery_btn.left:hover{
+            transform: translate(-5px, -50%) rotate(180deg);
+        }
+        .post_gallery_btn.right:hover{
+            transform: translateX(5px);
+        }
     </style>
 </head>
 
@@ -154,6 +288,46 @@
         <div class="sensor_div" id="shop_8_btn"></div>
         <div class="sensor_div" id="shop_9_btn"></div>
 
+
+    </div>
+    <div class="black_block">
+
+        <div class="blog_block">
+            <div class="top_button_wrap">
+                <div class="top_button active">Latest</div>
+                <div class="top_button">Popular</div>
+                <div class="top_button">Commented</div>
+            </div>
+            <div class="blog_list_wrap">
+                <div class="blog_post">
+                    <div class="post_gallery">
+                        <div class="post_gallery_image"></div>
+                        <div class="post_gallery_btn left"></div>
+                        <div class="post_gallery_btn right"></div>
+                    </div>
+                    <div class="post_text_block_wrap">
+                        <div class="tag_text">Fashion</div>
+                        <div class="post_title">
+                            William Saliba is Championing Unapologetic Individuality as Croc's Latest Ambassador
+                        </div>
+                        <div class="post_description">
+                            "Going off-pitch" is an opportunity to showcase who I am and my creativity through my fashion," the footballer says in an exclusive interview with Hypebeast.
+                        </div>
+                        <div class="post_change_button_wrap">
+                            <div class="post_change_button left"></div>
+                            <div class="post_change_button right"></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="footer_text">
+            Copyright © <?php echo date('Y'); ?>
+            All rights reserved.
+            <br>
+            Statue of Culture
+        </div>
     </div>
 
     <script>
@@ -167,6 +341,21 @@
             document.getElementById("shop_"+i+"_btn").addEventListener("mouseout", function(){
                 let img = document.getElementById("img_"+i);
                 img.style.opacity = "0";
+            });
+
+        }
+
+        {
+            let top_buttons = document.querySelectorAll('.top_button');
+            top_buttons.forEach(function (item) {
+
+                item.addEventListener('click', function () {
+                    top_buttons.forEach(function (item) {
+                        item.classList.remove('active');
+                    });
+                    this.classList.add('active');
+                });
+
             });
 
         }
